24aa8319f36cf99cbd4c55f05466ae7ba0f0674f
hserrano <hector.c.serrano@gmail.com>
2019-02-11 12:33:19 -0500

    tested hf 02-11-2019

--- ElctrcFldspy.py
+++ ElctrcFldspy.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 #Copyright 2017 Hector C Serrano
+#HF 02-11-2019
 #This file is part of Arrow Plot and Flux Lines
 #Arrow Plot and Flux Lines is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
 #the Free Software Foundation, either version 3 of the License, or any later version.
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Electric Fields Visualization using Python 2.7
 Requires tkinter
 #Commited with GIT
+#hf02-11-2019
